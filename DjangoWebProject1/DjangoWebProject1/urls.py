27e85ca12c87d4fbb9cfc9218d6d38ffbe3bd35d
Reljod Oreta <oretareljod@gmail.com>
2019-12-30 17:04:16 +0800

    Create initial Django App

--- DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -19,8 +19,10 @@ Including another URLconf
 # Uncomment next two lines to enable admin:
 #from django.contrib import admin
 #from django.urls import path
+from django.conf.urls import include, url
+import MyApp1.views
 
 urlpatterns = [
-    # Uncomment the next line to enable the admin:
-    #path('admin/', admin.site.urls)
+    url(r'^$', MyApp1.views.index, name='index'),
+    url(r'^home$', MyApp1.views.index, name='home')
 ]

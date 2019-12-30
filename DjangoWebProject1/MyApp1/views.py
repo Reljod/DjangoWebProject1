d4437721357d8b5f0f929bc723f7dca5ec9e21cf
Reljod Oreta <oretareljod@gmail.com>
2019-12-30 17:21:14 +0800

    Editing the index function in views.py to render.

--- DjangoWebProject1/MyApp1/views.py
+++ DjangoWebProject1/MyApp1/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "MyApp1/index.html",
+        {
+            "content": "<strong>Hello Django!</strong> on"
+            + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
